c8a87080f45a72ba9e5c9c4228434e0185dda086
LucasRe1s <lucaasreiis17@gmail.com>
2025-07-11 04:56:40 -0300

    add route principal

--- src/interface/health-check/health.routes.ts
+++ src/interface/health-check/health.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
 import healthController from "./health.controller";
-// healthController
-// import { AuthController } from "../controllers/auth.controller.js";
-// import { celebrate, Segments } from "celebrate";
-// import { authLoginSchema, authRecoverySchema } from "../models/user.model.js";
 
 export const healthRoute = Router();
 
+healthRoute.get ("/", expressAsyncHandler(healthController.healthCheck));
 healthRoute.get ("/health", expressAsyncHandler(healthController.healthCheck));
 
